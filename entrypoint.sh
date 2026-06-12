b93d7b167d8a688ad483a6155e4cbc02f2bc943d
LucianoEBC <beneventi.castillo@gmail.com>
2026-06-12 19:43:48 -0400

    fix: dockerFile

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "[Alembic] Ejecutando migraciones..."
-alembic upgrade head
-echo "[Alembic] Migraciones completadas"
-
-exec uvicorn app.main:app --host 0.0.0.0 --port 3004
